9c61451ee62073f91482d6a72b72fd422320ad54
if1live <libsora25@gmail.com>
2019-06-21 12:07:08 +0900

    feat: initial version

--- src/express-my-error-handler.ts
+++ src/express-my-error-handler.ts
@@ -1,6 +1,64 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-  // import "core-js/fn/array.find"
-  // ...
-export default class DummyClass {
+import express from 'express'
+import { ValidationError } from 'yup'
 
+type HttpError = Error & { statusCode: number }
+type StandardError = HttpError & { code: number }
+
+function isHttpError(x: any): x is HttpError {
+  return typeof x.statusCode === 'number'
+}
+
+function isStandardError(x: any): x is StandardError {
+  return typeof x.statusCode === 'number' && typeof x.code === 'number'
+}
+
+function isValidationError(x: any): x is ValidationError {
+  return x.name === 'ValidationError'
+}
+
+function getStatusCode(err: ErrorType) {
+  if (isHttpError(err)) {
+    return err.statusCode
+  } else if (isValidationError(err)) {
+    // Bad Request
+    return 400
+  } else {
+    // Internal Server Error
+    return 500
+  }
+}
+
+export interface ErrorResp {
+  name: string
+  message: string | undefined
+  code: number
+  stack: string[] | undefined
+}
+
+type ErrorType = Error | HttpError | ValidationError
+
+export const myErrorHandler = (production: boolean) => {
+  return (
+    err: ErrorType,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    const name = err.name
+    const message = production ? undefined : err.message
+    const statusCode = getStatusCode(err)
+    const stackStr = production ? undefined : err.stack
+    const stack = stackStr ? stackStr.split('\n') : undefined
+    const code = isStandardError(err) ? err.code : 0
+
+    const resp: ErrorResp = {
+      name,
+      message,
+      stack,
+      code
+    }
+
+    res.status(statusCode).json(resp)
+    next(err)
+  }
 }
--- test/express-my-error-handler.test.ts
+++ test/express-my-error-handler.test.ts
@@ -1,14 +1,140 @@
-import DummyClass from "../src/express-my-error-handler"
+import * as express from 'express'
+import * as faker from 'faker'
+import * as request from 'supertest'
+import * as yup from 'yup'
+import { myErrorHandler } from '../src/express-my-error-handler'
 
-/**
- * Dummy test
- */
-describe("Dummy test", () => {
-  it("works if true is truthy", () => {
-    expect(true).toBeTruthy()
+function makeApp(prod: boolean) {
+  const app = express()
+
+  app.get('/http/:statusCode', (req, res) => {
+    const e: any = new Error('sample http error')
+    e.name = 'HttpError'
+    e.statusCode = parseInt(req.params.statusCode, 10) || 400
+    throw e
   })
 
-  it("DummyClass is instantiable", () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  app.get('/standard/:statusCode/:code', (req, res) => {
+    const e: any = new Error('sample standard error')
+    e.name = 'StandardError'
+    e.statusCode = parseInt(req.params.statusCode, 10) || 400
+    e.code = parseInt(req.params.code, 10) || 1
+    throw e
+  })
+
+  app.get('/error', (req, res) => {
+    throw new Error('sample error')
+  })
+
+  app.get('/yup/', (req, res) => {
+    const schema = yup.object().shape({
+      str: yup.string().required()
+    })
+    const x = schema.validateSync({})
+    res.json({ x })
+  })
+
+  app.use('/', myErrorHandler(prod))
+
+  return app
+}
+
+const statusCode = faker.random.number({ min: 400, max: 499 })
+const code = faker.random.number({ min: 1, max: 100 })
+
+function assertRequest(opts: {
+  app: express.Application
+  url: string
+  statusCode: number
+  expected: object
+  done: jest.DoneCallback
+}) {
+  const { app, url, statusCode, expected, done } = opts
+
+  request(app)
+    .get(url)
+    .expect(statusCode)
+    .end((err, res) => {
+      if (err) {
+        throw err
+      }
+
+      const body = res.body
+      expect(Array.isArray(body.stack)).toBe(true)
+
+      delete body.stack
+      expect(body).toEqual(expected)
+      done()
+    })
+}
+
+describe('development', () => {
+  const production = false
+  const app = makeApp(production)
+
+  test('http error', done => {
+    const url = `/http/${statusCode}`
+    const expected = {
+      name: 'HttpError',
+      message: 'sample http error',
+      code: 0
+    }
+    assertRequest({ app, url, statusCode, expected, done })
+  })
+
+  test('standard error', done => {
+    const url = `/standard/${statusCode}/${code}`
+    const expected = {
+      name: 'StandardError',
+      message: 'sample standard error',
+      code
+    }
+    assertRequest({ app, url, statusCode, expected, done })
+  })
+
+  test('yup - validation error', done => {
+    const url = `/yup`
+    const expected = {
+      name: 'ValidationError',
+      message: 'str is a required field',
+      code: 0
+    }
+    assertRequest({ app, url, statusCode: 400, expected, done })
+  })
+
+  test('error', done => {
+    const url = `/error`
+    const expected = {
+      name: 'Error',
+      message: 'sample error',
+      code: 0
+    }
+    assertRequest({ app, url, statusCode: 500, expected, done })
+  })
+})
+
+describe('production', () => {
+  const production = true
+  const app = makeApp(production)
+
+  test('empty message', done => {
+    const url = `/http/${statusCode}`
+    const expected = {
+      name: 'HttpError',
+      code: 0,
+      message: undefined,
+      stack: undefined
+    }
+
+    request(app)
+      .get(url)
+      .expect(statusCode)
+      .end((err, res) => {
+        if (err) {
+          throw err
+        }
+        expect(res.body).toEqual(expected)
+        done()
+      })
   })
 })

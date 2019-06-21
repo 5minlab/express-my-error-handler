f408c6eb23706a9a9a5ef090358ca3a574163d31
if1live <libsora25@gmail.com>
2019-06-21 15:48:59 +0900

    feat: support status/statusCode

--- src/express-my-error-handler.ts
+++ src/express-my-error-handler.ts
@@ -1,31 +1,39 @@
 import express from 'express'
-import { ValidationError } from 'yup'
 
-type HttpError = Error & { statusCode: number }
-type StandardError = HttpError & { code: number }
+// express에서는 일반적으로 에러에 붙은 statusCode를 http status로 쓴다
+// https://gist.github.com/zcaceres/2854ef613751563a3b506fabce4501fd
+type StatusCodeError = Error & { statusCode: number }
 
-function isHttpError(x: any): x is HttpError {
+// https://github.com/auth0/express-jwt/blob/5766a24aeb7db15b8a183c59b4a9145552702f0e/lib/errors/UnauthorizedError.js
+// 일부 라이브러리는 status를 쓴다
+type StatusError = Error & { status: number }
+
+function isStatusCodeError(x: any): x is StatusCodeError {
   return typeof x.statusCode === 'number'
 }
-
-function isStandardError(x: any): x is StandardError {
-  return typeof x.statusCode === 'number' && typeof x.code === 'number'
-}
-
-function isValidationError(x: any): x is ValidationError {
-  return x.name === 'ValidationError'
+function isStatusError(x: any): x is StatusError {
+  return typeof x.status === 'number'
 }
 
 function getStatusCode(err: ErrorType) {
-  if (isHttpError(err)) {
+  // status가 정의된 경우
+  if (isStatusCodeError(err)) {
     return err.statusCode
-  } else if (isValidationError(err)) {
-    // Bad Request
-    return 400
-  } else {
-    // Internal Server Error
-    return 500
+  } else if (isStatusError(err)) {
+    return err.status
   }
+
+  // name based
+  if (err.name === 'ValidationError') {
+    return 400
+  }
+
+  // else...
+  return 500
+}
+
+function getCode(x: any): number {
+  return typeof x.code === 'number' ? x.code : 0
 }
 
 export interface ErrorResp {
@@ -35,7 +43,7 @@ export interface ErrorResp {
   stack: string[] | undefined
 }
 
-type ErrorType = Error | HttpError | ValidationError
+type ErrorType = Error | StatusCodeError | StatusError
 
 export const myErrorHandler = (production: boolean) => {
   return (
@@ -49,7 +57,7 @@ export const myErrorHandler = (production: boolean) => {
     const statusCode = getStatusCode(err)
     const stackStr = production ? undefined : err.stack
     const stack = stackStr ? stackStr.split('\n') : undefined
-    const code = isStandardError(err) ? err.code : 0
+    const code = getCode(err)
 
     const resp: ErrorResp = {
       name,
--- test/express-my-error-handler.test.ts
+++ test/express-my-error-handler.test.ts
@@ -1,155 +1,120 @@
 import * as express from 'express'
-import * as faker from 'faker'
 import * as request from 'supertest'
-import * as yup from 'yup'
 import { myErrorHandler } from '../src/express-my-error-handler'
 
-function makeApp(prod: boolean) {
-  const app = express()
-
-  app.get('/http/:statusCode', (req, res) => {
-    const e: any = new Error('sample http error')
-    e.name = 'HttpError'
-    e.statusCode = parseInt(req.params.statusCode, 10) || 400
-    throw e
-  })
-
-  app.get('/standard/:statusCode/:code', (req, res) => {
-    const e: any = new Error('sample standard error')
-    e.name = 'StandardError'
-    e.statusCode = parseInt(req.params.statusCode, 10) || 400
-    e.code = parseInt(req.params.code, 10) || 1
-    throw e
-  })
-
-  app.get('/error', (req, res) => {
-    throw new Error('sample error')
-  })
-
-  app.get('/custom', (req, res) => {
-    const message = req.query.message
-    const name = req.query.name
-    const statusCode = parseInt(req.query.statusCode || '500', 10)
-    const e: any = new Error(message)
-    e.name = name
-    e.statusCode = statusCode
-    throw e
-  })
-
-  app.get('/yup/', (req, res) => {
-    const schema = yup.object().shape({
-      str: yup.string().required()
-    })
-    const x = schema.validateSync({})
-    res.json({ x })
-  })
-
-  app.use('/', myErrorHandler(prod))
-
-  return app
-}
-
-const statusCode = faker.random.number({ min: 400, max: 499 })
-const code = faker.random.number({ min: 1, max: 100 })
-
-function assertRequest(opts: {
-  test: request.Test
-  statusCode: number
-  expected: object
-  done: jest.DoneCallback
+function makeHandler(opts: {
+  name?: string
+  message?: string
+  code?: number
+  status?: number
+  statusCode?: number
 }) {
-  const { test, statusCode, expected, done } = opts
+  const message = opts.message ? opts.message : ''
+  const keys: Array<keyof typeof opts> = ['name', 'code', 'status', 'statusCode']
 
-  test.expect(statusCode).end((err, res) => {
-    if (err) {
-      throw err
+  return (req: express.Request, res: express.Response) => {
+    const e: any = new Error(message)
+    for (const key of keys) {
+      const val = opts[key]
+      if (val) {
+        e[key] = val
+      }
     }
-
-    const body = res.body
-    expect(Array.isArray(body.stack)).toBe(true)
-
-    delete body.stack
-    expect(body).toEqual(expected)
-    done()
-  })
+    throw e
+  }
 }
 
-describe('development', () => {
-  const production = false
-  const app = makeApp(production)
+describe('error', () => {
+  const app = express()
+  app.get('/', makeHandler({}))
+  app.use(myErrorHandler(false))
 
-  test('http error', done => {
-    const url = `/http/${statusCode}`
-    const expected = {
-      name: 'HttpError',
-      message: 'sample http error',
-      code: 0
-    }
-    const test = request(app).get(url)
-    assertRequest({ test, statusCode, expected, done })
+  test('ok', done => {
+    request(app)
+      .get('/')
+      .expect(500)
+      .end((err, res) => {
+        if (err) {
+          throw err
+        }
+        expect(res.body.code).toBe(0)
+        done()
+      })
   })
+})
 
-  test('standard error', done => {
-    const url = `/standard/${statusCode}/${code}`
-    const expected = {
-      name: 'StandardError',
-      message: 'sample standard error',
-      code
-    }
-    const test = request(app).get(url)
-    assertRequest({ test, statusCode, expected, done })
-  })
+describe('with code', () => {
+  const code = 123
+  const app = express()
+  app.get('/', makeHandler({ code }))
+  app.use(myErrorHandler(false))
 
-  test('yup - validation error', done => {
-    const url = `/yup`
-    const expected = {
-      name: 'ValidationError',
-      message: 'str is a required field',
-      code: 0
-    }
-    const test = request(app).get(url)
-    assertRequest({ test, statusCode: 400, expected, done })
+  test('ok', done => {
+    request(app)
+      .get('/')
+      .expect(500)
+      .end((err, res) => {
+        if (err) {
+          throw err
+        }
+        expect(res.body.code).toBe(code)
+        done()
+      })
   })
+})
 
-  test('error', done => {
-    const url = `/error`
-    const expected = {
-      name: 'Error',
-      message: 'sample error',
-      code: 0
-    }
-    const test = request(app).get(url)
-    assertRequest({ test, statusCode: 500, expected, done })
-  })
+describe('with statusCode', () => {
+  const statusCode = 412
+  const app = express()
+  app.get('/', makeHandler({ statusCode }))
+  app.use(myErrorHandler(false))
 
-  test('UnauthorizedError', done => {
-    const name = 'UnauthorizedError'
-    const message = 'No authorization token was found'
-    const statusCode = 401
-    const expected = { name, message, code: 0 }
-    const test = request(app)
-      .get('/custom')
-      .query({ name, message, statusCode })
-    assertRequest({ test, statusCode, expected, done })
-  })
+  test('ok', done =>
+    request(app)
+      .get('/')
+      .expect(statusCode, done))
+})
+
+describe('with status', () => {
+  const status = 413
+  const app = express()
+  app.get('/', makeHandler({ status }))
+  app.use(myErrorHandler(false))
+
+  test('ok', done =>
+    request(app)
+      .get('/')
+      .expect(status, done))
+})
+
+describe('validation error', () => {
+  const app = express()
+  app.get('/', makeHandler({ name: 'ValidationError', message: '' }))
+  app.use(myErrorHandler(false))
+
+  test('ok', done =>
+    request(app)
+      .get('/')
+      .expect(400, done))
 })
 
 describe('production', () => {
   const production = true
-  const app = makeApp(production)
+  const app = express()
+  app.get('/', makeHandler({ name: 'sample error', message: 'sample message' }))
+  app.use(myErrorHandler(production))
 
-  test('empty message', done => {
-    const url = `/http/${statusCode}`
+  test('empty message, empty stack', done => {
     const expected = {
-      name: 'HttpError',
+      name: 'sample error',
       code: 0,
       message: undefined,
       stack: undefined
     }
 
     request(app)
-      .get(url)
-      .expect(statusCode)
+      .get('/')
+      .expect(500)
       .end((err, res) => {
         if (err) {
           throw err
